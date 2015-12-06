31e6113525cc8e4a6f91b2f6235293fdca72c098
kylewray <kyle.hollins.wray@gmail.com>
2015-12-06 15:32:21 -0500

    Revised batch domains, and structure of tests.

---
+++ tests/pomdp/batch/algorithms/algorithms.py
@@ -0,0 +1,118 @@
+""" The MIT License (MIT)
+
+    Copyright (c) 2015 Kyle Hollins Wray, University of Massachusetts
+
+    Permission is hereby granted, free of charge, to any person obtaining a copy of
+    this software and associated documentation files (the "Software"), to deal in
+    the Software without restriction, including without limitation the rights to
+    use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+    the Software, and to permit persons to whom the Software is furnished to do so,
+    subject to the following conditions:
+
+    The above copyright notice and this permission notice shall be included in all
+    copies or substantial portions of the Software.
+
+    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+    FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+    COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+    IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+    CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+"""
+
+import os
+import sys
+
+thisFilePath = os.path.dirname(os.path.realpath(__file__))
+
+sys.path.append(os.path.join(thisFilePath, "..", "..", "..", "..", "python"))
+from nova.pomdp import *
+
+from pylab import *
+
+
+horizon = 50
+numTrials = 10
+
+algorithms = ['pbvi', 'perseus']
+fixedProcess = 'cpu'
+
+files = [
+        {'name': "tiger", 'filename': "domains/tiger_95.pomdp", 'filetype': "pomdp", 'numExpandSteps': 4},
+        #{'name': "shuttle", 'filename': "domains/shuttle_95.pomdp", 'filetype': "pomdp", 'numExpandSteps': 4},
+        #{'name': "paint", 'filename': "domains/paint_95.pomdp", 'filetype': "pomdp", 'numExpandSteps': 4},
+        #{'name': "grid-4x3", 'filename': "domains/4x3_95.pomdp", 'filetype': "pomdp", 'numExpandSteps': 5},
+        #{'name': "tiger-grid", 'filename': "domains/tiger_grid.pomdp", 'filetype': "pomdp",'numExpandSteps': 6},
+        #{'name': "aloha-10", 'filename': "domains/aloha_10.pomdp", 'filetype': "pomdp", 'numExpandSteps': 6},
+        #{'name': "hallway2", 'filename': "domains/hallway2.pomdp", 'filetype': "pomdp", 'numExpandSteps': 7},
+        #{'name': "aloha-30", 'filename': "domains/aloha_30.pomdp", 'filetype': "pomdp", 'numExpandSteps': 7},
+        #{'name': "tag", 'filename': "domains/tag.pomdp", 'filetype': "pomdp", 'numExpandSteps': 8},
+        #{'name': "fourth", 'filename': "domains/fourth.pomdp", 'filetype': "pomdp", 'numExpandSteps': 8},
+        #{'name': "rock-sample (7x8)", 'filename': "domains/rockSample_7_8.pomdp", 'filetype': "pomdp", 'numExpandSteps': 9},
+        #{'name': "auv-navigation", 'filename': "domains/auvNavigation.pomdp", 'filetype': "pomdp", 'numExpandSteps': 10},
+
+        #{'name': "drive_san_francisco", 'filename': "domains/drive_san_francisco.pomdp", 'filetype': "pomdp", 'numExpandSteps': 8},
+        #{'name': "drive_seattle", 'filename': "domains/drive_seattle.pomdp", 'filetype': "pomdp", 'numExpandSteps': 9},
+        #{'name': "drive_new_york_city", 'filename': "domains/drive_new_york_city.pomdp", 'filetype': "pomdp", 'numExpandSteps': 9},
+        #{'name': "drive_boston", 'filename': "domains/drive_boston.pomdp", 'filetype': "pomdp", 'numExpandSteps': 10},
+        ]
+
+
+for f in files:
+    print(f['name'])
+
+    filename = os.path.join(thisFilePath, f['filename'])
+
+    for a in algorithms:
+        print(" - %s " % (a), end='')
+
+        fileSuffix = "_".join([f['name'], a])
+
+        with open(os.path.join(thisFilePath, "results", fileSuffix) + ".csv", "w") as out:
+            out.write("n,m,z,r,ns,rz,time,V(b0)\n")
+
+            for j in range(numTrials):
+                print(".", end='')
+                sys.stdout.flush()
+
+                pomdp = POMDP()
+                pomdp.load(filename, filetype=f['filetype'])
+                pomdp.horizon = int(horizon)
+
+                # Store the intial belief from this file.
+                rzOriginal = pomdp.rz
+                b0 = [pomdp.B[0 * pomdp.rz + k] for k in range(pomdp.rz)]
+                z0 = [pomdp.Z[0 * pomdp.rz + k] for k in range(pomdp.rz)]
+
+                # Do expansions using random exploration. This lets us control the exact number of beliefs for
+                # CPU vs GPU. Note: The number of beliefs is likely to be way too small for random exploration.
+                # This is just to get a sense of the speed, not the optimal value. Also, the horizon is much
+                # too small for the larger domains.
+                pomdp.expand(method='random', numBeliefsToAdd=(pow(2, f['numExpandSteps']) - 1))
+
+                Gamma, piResult, timing = pomdp.solve(process=fixedProcess, algorithm=a)
+
+                #print(pomdp)
+                #print(Gamma)
+                #print(piResult)
+
+                # Compute the value of the initial belief, stored before doing the sigma approximation.
+                Vb0 = pomdp.Rmin / (1.0 - pomdp.gamma)
+                for q in range(Gamma.shape[0]):
+                    Vb0q = 0.0
+
+                    for k in range(rzOriginal):
+                        s = z0[k]
+                        if s < 0:
+                            break
+                        Vb0q += b0[k] * Gamma[q, s]
+
+                    if Vb0 < Vb0q:
+                        Vb0 = Vb0q
+
+                # Note: use the time.time() function, which measures wall-clock time.
+                out.write("%i,%i,%i,%i,%i,%i,%.5f,%.5f\n" % (pomdp.n, pomdp.m, pomdp.z, pomdp.r, pomdp.ns, pomdp.rz,
+                                                            timing[0], Vb0))
+
+        print()
+
---
+++ tests/pomdp/tiger/tiger.py
@@ -0,0 +1,94 @@
+""" The MIT License (MIT)
+
+    Copyright (c) 2015 Kyle Hollins Wray, University of Massachusetts
+
+    Permission is hereby granted, free of charge, to any person obtaining a copy of
+    this software and associated documentation files (the "Software"), to deal in
+    the Software without restriction, including without limitation the rights to
+    use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+    the Software, and to permit persons to whom the Software is furnished to do so,
+    subject to the following conditions:
+
+    The above copyright notice and this permission notice shall be included in all
+    copies or substantial portions of the Software.
+
+    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+    FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+    COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+    IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+    CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+"""
+
+import os
+import sys
+import pylab
+
+thisFilePath = os.path.dirname(os.path.realpath(__file__))
+
+sys.path.append(os.path.join(thisFilePath, "..", "..", "..", "python"))
+from nova.mdp import *
+from nova.pomdp import *
+
+
+files = [
+        {'filename': "tiger_pomdp.raw", 'filetype': "raw", 'process': 'gpu', 'algorithm': 'pbvi', 'expand': None},
+        {'filename': "tiger_95.pomdp", 'filetype': "pomdp", 'process': 'gpu', 'algorithm': 'pbvi', 'expand': "random"},
+        {'filename': "tiger_95.pomdp", 'filetype': "pomdp", 'process': 'gpu', 'algorithm': 'pbvi', 'expand': "distinct_beliefs"},
+        {'filename': "tiger_95.pomdp", 'filetype': "pomdp", 'process': 'gpu', 'algorithm': 'pbvi', 'expand': "pema"},
+
+        {'filename': "tiger_pomdp.raw", 'filetype': "raw", 'process': 'cpu', 'algorithm': 'pbvi', 'expand': None},
+        {'filename': "tiger_95.pomdp", 'filetype': "pomdp", 'process': 'cpu', 'algorithm': 'pbvi', 'expand': "random"},
+        {'filename': "tiger_95.pomdp", 'filetype': "pomdp", 'process': 'cpu', 'algorithm': 'pbvi', 'expand': "distinct_beliefs"},
+        {'filename': "tiger_95.pomdp", 'filetype': "pomdp", 'process': 'cpu', 'algorithm': 'pbvi', 'expand': "pema"},
+
+        #{'filename': "tiger_pomdp.raw", 'filetype': "raw", 'process': 'gpu', 'algorithm': 'perseus', 'expand': None},
+        #{'filename': "tiger_95.pomdp", 'filetype': "pomdp", 'process': 'gpu', 'algorithm': 'perseus', 'expand': "random"},
+        #{'filename': "tiger_95.pomdp", 'filetype': "pomdp", 'process': 'gpu', 'algorithm': 'perseus', 'expand': "distinct_beliefs"},
+        #{'filename': "tiger_95.pomdp", 'filetype': "pomdp", 'process': 'gpu', 'algorithm': 'perseus', 'expand': "pema"},
+
+        {'filename': "tiger_pomdp.raw", 'filetype': "raw", 'process': 'cpu', 'algorithm': 'perseus', 'expand': None},
+        {'filename': "tiger_95.pomdp", 'filetype': "pomdp", 'process': 'cpu', 'algorithm': 'perseus', 'expand': "random"},
+        {'filename': "tiger_95.pomdp", 'filetype': "pomdp", 'process': 'cpu', 'algorithm': 'perseus', 'expand': "distinct_beliefs"},
+        {'filename': "tiger_95.pomdp", 'filetype': "pomdp", 'process': 'cpu', 'algorithm': 'perseus', 'expand': "pema"},
+        ]
+
+for f in files:
+    tigerFile = os.path.join(thisFilePath, f['filename'])
+    tiger = POMDP()
+    tiger.load(tigerFile, filetype=f['filetype'])
+    print(tiger)
+
+    if f['expand'] == "random":
+        # Note: 1 + 250 = 251 belief points.
+        tiger.expand(method=f['expand'], numBeliefsToAdd=250)
+        print(tiger)
+    elif f['expand'] == "distinct_beliefs":
+        # Note: 2^3 = 8 belief points.
+        for i in range(3):
+            tiger.expand(method=f['expand'])
+        print(tiger)
+    elif f['expand'] == "pema":
+        # Note: 1 + 4 = 5 belief points.
+        for i in range(4):
+            tiger.expand(method=f['expand'])
+            print(tiger)
+
+    #Gamma, piResult, timing = tiger.solve()
+    Gamma, piResult, timing = tiger.solve(process=f['process'], algorithm=f['algorithm'])
+    print(Gamma)
+    print(piResult)
+
+    pylab.hold(True)
+    pylab.title("Alpha-Vectors for Tiger Problem (Expand: %s)" % (f['expand']))
+    pylab.xlabel("Belief of State s2: b(s2)")
+    pylab.ylabel("Value of Belief: V(b(s2))")
+    for i in range(len(piResult)):
+        if piResult[i] == 0:
+            pylab.plot([0.0, 1.0], [Gamma[i, 0], Gamma[i, 1]], linewidth=10, color='red')
+        elif piResult[i] == 1:
+            pylab.plot([0.0, 1.0], [Gamma[i, 0], Gamma[i, 1]], linewidth=10, color='green')
+        elif piResult[i] == 2:
+            pylab.plot([0.0, 1.0], [Gamma[i, 0], Gamma[i, 1]], linewidth=10, color='blue')
+    pylab.show()
+
