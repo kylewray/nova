c2c1b015a12f1bb8051f5101e555e04d4c99f551
Kyle Hollins Wray <kyle.hollins.wray@gmail.com>
2017-11-14 17:11:47 -0500

    More structure for POMDP NLP solution.

---
+++ include/nova/pomdp/algorithms/pomdp_nlp_cpu.h
@@ -0,0 +1,90 @@
+/**
+ *  The MIT License (MIT)
+ *
+ *  Copyright (c) 2017 Kyle Hollins Wray, University of Massachusetts
+ *
+ *  Permission is hereby granted, free of charge, to any person obtaining a copy of
+ *  this software and associated documentation files (the "Software"), to deal in
+ *  the Software without restriction, including without limitation the rights to
+ *  use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+ *  the Software, and to permit persons to whom the Software is furnished to do so,
+ *  subject to the following conditions:
+ *
+ *  The above copyright notice and this permission notice shall be included in all
+ *  copies or substantial portions of the Software.
+ *
+ *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ *  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+ *  FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+ *  COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+ *  IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+ *  CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+
+#ifndef NOVA_POMDP_NLP_CPU_H
+#define NOVA_POMDP_NLP_CPU_H
+
+
+#include <nova/pomdp/pomdp.h>
+#include <nova/pomdp/policies/pomdp_stochastic_fsc.h>
+
+namespace nova {
+
+/**
+ *  The necessary variables to perform non-linear programming (NLP) on a POMDP within nova.
+ *  @param  k   The number of controller nodes desired in the final policy.
+ */
+typedef struct NovaPOMDPNLPCPU {
+    unsigned int k;
+} POMDPNLPCPU;
+
+/**
+ *  Execute all the NLP steps for the infinite horizon POMDP model specified.
+ *  @param  pomdp   The POMDP object.
+ *  @param  nlp     The POMDPNLPCPU object containing algorithm variables.
+ *  @param  policy  The resultant controller node probabilities. This will be modified.
+ *  @return Returns zero upon success, non-zero otherwise.
+ */
+extern "C" int pomdp_nlp_execute_cpu(const POMDP *pomdp, POMDPNLPCPU *nlp, POMDPStochasticFSC *policy);
+
+/**
+ *  Step 1/3: The initialization step of the NLP solution. This saves the POMDP as an AMLP data file.
+ *  @param  pomdp   The POMDP object.
+ *  @param  nlp     The POMDPNLPCPU object containing algorithm variables.
+ *  @return Returns zero upon success, non-zero otherwise.
+ */
+extern "C" int pomdp_nlp_initialize_cpu(const POMDP *pomdp, POMDPNLPCPU *nlp);
+
+/**
+ *  Step 2/3: Perform the solving of the NLP. There is only one call to update for the NLP solution.
+ *  @param  pomdp   The POMDP object.
+ *  @param  nlp     The POMDPNLPCPU object containing algorithm variables.
+ *  @return Returns zero upon success, non-zero otherwise.
+ */
+extern "C" int pomdp_nlp_update_cpu(const POMDP *pomdp, POMDPNLPCPU *nlp);
+
+/**
+ *  Step 3/4: The get resultant policy step of the NLP solution. This parses the result of the solver(s).
+ *  @param  pomdp   The POMDP object.
+ *  @param  nlp     The POMDPNLPCPU object containing algorithm variables.
+ *  @param  policy  The resultant controller node probabilities. This will be modified.
+ *  @return Returns zero upon success, non-zero otherwise.
+ */
+extern "C" int pomdp_nlp_get_policy_cpu(const POMDP *pomdp, POMDPNLPCPU *pbvi, POMDPStochasticFSC *policy);
+
+/**
+ *  Step 4/4: The uninitialization step of PBVI. This frees variables and removes temporary files.
+ *  @param  pomdp   The POMDP object.
+ *  @param  nlp     The POMDPNLPCPU object containing algorithm variables.
+ *  @param  policy  The resultant controller node probabilities. This will be modified.
+ *  @return Returns zero upon success, non-zero otherwise.
+ */
+extern "C" int pomdp_nlp_uninitialize_cpu(const POMDP *pomdp, POMDPNLPCPU *nlp);
+
+};
+
+ 
+#endif // NOVA_POMDP_NLP_CPU_H
+
+
